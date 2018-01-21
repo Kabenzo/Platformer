17af43a3c75b0a2ed0a7a396b9a6bde7e6a341cc
Kabenzo <enzoschlatter@gmail.com>
2018-01-20 22:27:25 -0800

    Lots of shit

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -14,29 +14,74 @@ public class PlayerController : MonoBehaviour {
 	public LayerMask whatIsGround;
 	public bool isGrounded;
 
+	public Animator PlayerAnim;
+
 	void Start () {
 		rbody = GetComponent<Rigidbody2D> ();
+		PlayerAnim = GetComponent<Animator> ();
+
 		
 	}
 
 
 	void Update () {
 
+		//Check if Player is on the ground
 		isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
 
+		#region Movement and Animations
+		//Movement and Animations
 		if (Mathf.Abs(Input.GetAxisRaw ("Horizontal")) != 0f) {
-			rbody.velocity = new Vector3 (moveSpeed * Input.GetAxis ("Horizontal"), rbody.velocity.y, 0f);
+			rbody.velocity = new Vector3 (moveSpeed * Input.GetAxisRaw ("Horizontal"), rbody.velocity.y, 0f);
+
+			//Flips Character Towards Direction he is moving in
+			if (Input.GetAxisRaw ("Horizontal") == 1f) {
+			transform.eulerAngles = new Vector3 (0,0,0);
+			}
+			else if (Input.GetAxisRaw ("Horizontal") == -1f) {
+				transform.eulerAngles = new Vector3 (0,180,0);
+			}
+
+			//Sets Walking animation if player is moving and on the ground
+			if (isGrounded == true) {
+				PlayerAnim.SetBool ("IsWalking", true);
+			}
 
 		} else {
 
 			rbody.velocity = new Vector3 (0f, rbody.velocity.y, 0f);
+			PlayerAnim.SetBool ("IsWalking", false);
 		}
+
 			
 
 		if (Input.GetButtonDown ("Jump") && isGrounded) {
-
 			rbody.velocity = new Vector3 (rbody.velocity.x, jumpSpeed, 0f);
+			PlayerAnim.SetBool ("IsWalking", false);
+
 		}
+
+		//Check if Jumping
+		if (isGrounded == true) {
+			PlayerAnim.SetBool ("IsJumping", false);
+		}
+		else if (Mathf.Sign (rbody.velocity.y) == 1) {
+			PlayerAnim.SetBool ("IsJumping", true);
+		}
+
+
+		//Check if Falling
+		 if (Mathf.Sign (rbody.velocity.y) == -1f) {
+			PlayerAnim.SetBool ("IsFalling", true);
+
+		} else {
+			PlayerAnim.SetBool ("IsFalling", false);
+
+		}
+
+
+		#endregion 
+
 
 
 
